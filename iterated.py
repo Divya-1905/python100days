89598c90a375c59b52f1827fbbec6b86ec2fecc1
admin <divyasri1905@gmail.com>
2022-12-08 22:33:34 +0530

    update

--- iterated.py
+++
@@ -1,1 +0,0 @@
-#iteraed is check one by one 
---
+++ tenary.py
@@ -0,0 +1,44 @@
+is_migician = False
+is_expert =False
+if is_migician:
+    print('your migician')
+elif is_expert:
+    print('your master')
+else :
+    print('your a student') 
+        # section 67 - the == is check a 1==0 is vaildated the value    
+print(1==0)
+print('1' is '1') # the  is check memory of 1 is i its xerorx of vale 
+print('1'is 0) # Its return false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# is_friend = False
+# # canmessage = "allowed" 
+# if is_friend :
+#     print('allowed')
+# else:
+#     print('not') 
+# apple_blue = True
+# cake_blue = False
+# if apple_blue and cake_blue:
+#     print('woow')
+# else :
+#     print('erw')    
+        
+
+      
