c974efa422b692662dce0908c4b29f0f89c1cd1b
admin <divyasri1905@gmail.com>
2022-12-11 19:16:35 +0530

    new

---
+++ docstring.py
@@ -0,0 +1,9 @@
+def fun(w):
+    '''
+    sdfsdfdsfd asdfgadsfghlkj
+    '''
+
+    print(w)
+print(fun.__doc__)
+#this __doc__  return a command ''' line of dots
+   
---
+++ scope.py
@@ -0,0 +1,12 @@
+# a =1
+# def first():
+#     a = 3
+#     return a 
+# print(first())
+# print(a)
+b = 34
+def second():
+    b = 35
+    return b
+print(second())
+print(b)
