849a172f7e6480f2bbc23d9a0a7e5a142b94f8de
admin <divyasri1905@gmail.com>
2022-12-15 21:10:53 +0530

    update

--- class.py
+++ class.py
@@ -11,11 +11,13 @@ class student():
         self.address = address
         self.pincode = 62505
         self.phonenumber = 45352398
+        self.standard = 3
+        
          # the defult value assign parameter is not called 
         
 
     def run(self):
-        print(f'name is {self.name},registernumber is{self.registernumber},section is {self.section},address is {self.address},pincode is {self.pincode} the phone number is {self.phonenumber}')
+        print(f'name is {self.name},registernumber is{self.registernumber},section is {self.section},address is {self.address},pincode is {self.pincode} the phone number is {self.phonenumber}, i am studying in {self.standard}')
         
 class regbook(student):
     def __init__(self, name, section, registernumber,address):
@@ -24,6 +26,7 @@ class regbook(student):
         self.registernumber = registernumber
         super().__init__(name,section,registernumber,address)
         self.pincode = 625034
+        
 take = student('rio','e',19034,'madurai')  
 print(take.name) 
 take.run()
---
+++ classzoo.py
@@ -0,0 +1,8 @@
+zoo = str(input('which animal you add this zoo')) 
+animal = []
+birds = []
+if (zoo=='lion'):
+    for i in zoo:
+        animal.append()
+print(zoo)  
+
